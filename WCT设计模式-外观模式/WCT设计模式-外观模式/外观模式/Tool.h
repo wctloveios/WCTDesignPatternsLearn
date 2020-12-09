f96b2274d8f9e8ae1587365e6b788dceb65b01f1
wangchuntao <wctloveios@163.com>
2020-12-09 22:05:28 +0800

    外观模式解读+简单案例

---
+++ WCT设计模式-外观模式/WCT设计模式-外观模式/外观模式/Tool.h
@@ -0,0 +1,24 @@
+//
+//  Tool.h
+//  WCT设计模式-外观模式
+//
+//  Created by wct on 2020/12/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Tool : NSObject
+
++ (void)runing;
+
++ (void)cooking;
+
++ (void)shopping;
+
++ (void)sleeping;
+
+@end
+
+NS_ASSUME_NONNULL_END
